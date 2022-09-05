a03e096612e87e6b014b8be45d76aca81effd498
Kirill <ksfomin1315@gmail.com>
2022-09-05 23:40:31 +0300

    task2

---
+++ homework/task2/Program.cs
@@ -0,0 +1,9 @@
+﻿// task2
+int numberA = 22;
+int numberB = 3;
+int numberC = 9;
+int max=numberA;
+if (numberB > max)  max = numberB;
+if (numberC > max)  max = numberC;
+Console.Write ("max = ");
+Console.WriteLine(max);
