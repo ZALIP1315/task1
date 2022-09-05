9250b46afd459aa6fb827f15393633e46bd7fe07
Kirill <ksfomin1315@gmail.com>
2022-09-05 23:38:40 +0300

    task3

---
+++ homework/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// task3
+int numberA = 7;
+int numberM = numberA /2;
+int numberN = numberM *2;
+if (numberA == numberN)
+{
+  Console.WriteLine("да,число четное");  
+}
+else 
+{
+    Console.WriteLine("нет,число нечетное");
+}
