a377cff686a36d8b8794e4316d26f61103278e64
Kirill <ksfomin1315@gmail.com>
2022-09-05 21:53:23 +0300

    task1

---
+++ homework/task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Task 1
+int numberA = -9;
+int numberB = -3;
+int min=numberA;
+int max=numberB;
+if (numberA > numberB)  max = numberA; 
+if (numberA > numberB)  min = numberB;
+Console.Write ("max = ");
+Console.WriteLine(max);
+Console.Write ("min = ");
+Console.WriteLine(min);
