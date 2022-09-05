61de8ca47cb6c3622b0d333dfe1bb342490d166b
Kirill <ksfomin1315@gmail.com>
2022-09-06 00:31:40 +0300

    task4

---
+++ homework/task4/Program.cs
@@ -0,0 +1,24 @@
+﻿// task4
+int numberN = 8;
+int numberX = numberN /2;
+int numberY = numberX *2;
+int numberA = 2;
+
+if (numberY == numberN)
+{
+numberN = numberN;  
+}
+else 
+{
+ numberN = numberN -1;
+}
+
+while (numberA <= numberN)
+{
+Console.WriteLine(numberA);
+numberA = numberA+2;  
+}
+ 
+
+ 
+
