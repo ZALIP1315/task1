6a1e7894f70032596b5ca2e0896139ba8be35e6b
Kirill <ksfomin1315@gmail.com>
2022-09-08 22:45:09 +0300

    tak13

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿//task 13
+Console.WriteLine("Введите число"); 
+int N1 = Convert.ToInt32(Console.ReadLine());
+string N2 = Convert.ToString(N1);
+if (N2.Length > 2)
+{
+    Console.WriteLine(N2[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
