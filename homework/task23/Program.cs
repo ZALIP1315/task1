5eac0ba90d9828a48861615814841ef098e2fab7
Kirill <ksfomin1315@gmail.com>
2022-09-13 14:36:23 +0300

    task23

---
+++ homework/task23/Program.cs
@@ -0,0 +1,10 @@
+﻿// task23
+Console.WriteLine("Введите число");
+int number= Convert.ToInt32(Console.ReadLine());
+if (number >0)
+{
+    for(int i=1;i<=number;i++)
+    Console.Write(i * i * i + " " );
+}
+else
+Console.WriteLine("Неправильно введено число");
