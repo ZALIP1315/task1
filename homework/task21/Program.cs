7f0c80395c45f46fec92ae102ec856022c732a61
Kirill <ksfomin1315@gmail.com>
2022-09-13 19:11:47 +0300

    task21

---
+++ homework/task21/Program.cs
@@ -0,0 +1,15 @@
+﻿// tak21
+Console.WriteLine("Введите координату точки А по x");
+int Ax= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки А по y");
+int Ay= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки А по z");
+int Az= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки B по x");
+int Bx= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки B по y");
+int By= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки B по z");
+int Bz= Convert.ToInt32(Console.ReadLine());
+double rez= Math.Sqrt(Math.Pow(Bx-Ax, 2)+ Math.Pow(By-Ay, 2) +Math.Pow(Bz-Az, 2));
+Console.WriteLine($"{rez:f2}");
