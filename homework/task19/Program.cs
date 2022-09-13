9bddfb98abf11574b0e81622574a0e62c946679b
Kirill <ksfomin1315@gmail.com>
2022-09-13 14:16:10 +0300

    task19

---
+++ homework/task19/Program.cs
@@ -0,0 +1,15 @@
+﻿// task19
+
+Console.WriteLine("Введите число"); 
+String number = Console.ReadLine();
+if (number.Length ==5)
+{
+    if(number[0]==number[4] && number[1]==number[3])
+    Console.WriteLine($"{number}-паллиндром");
+    else
+    Console.WriteLine($"{number}-не паллиндром");
+}
+else 
+{
+    Console.WriteLine ("Вы ввели неправильное число");
+};
