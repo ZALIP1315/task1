7e6bb2f42922643e9d80aac028cc6ebab2b589c0
Kirill <ksfomin1315@gmail.com>
2022-09-20 17:26:24 +0300

    task25

---
+++ homework/task25/Program.cs
@@ -0,0 +1,18 @@
+﻿// task25
+Console.WriteLine("Введите число 1");
+int number= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int number2= Convert.ToInt32(Console.ReadLine());
+int res = 1;
+if (number >= 1 & number2 >= 1) 
+{
+    for (int i=1;i<=number2;i++)
+    {
+        res = res*number;
+    }
+    Console.WriteLine($"{res}");
+}
+else 
+{
+    Console.WriteLine("Введено некоректное число");
+};
